f824ec03a303a87c3f9feb2620d829ad6be06bed
DaanBelt <daniel.belt@live.nl>
2025-01-06 09:05:16 +0100

    7. Vervaardig de KitchenType class #7

---
+++ lib/kitchentype.php
@@ -0,0 +1,23 @@
+<?php
+
+class kitchentype {
+
+    private $connection;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+  
+    public function selectKitchentype($kitchentype_id) {
+
+        $sql = "select * from kitchentype where id = $kitchentype_id";
+        
+        $result = mysqli_query($this->connection, $sql);
+        $kitchentype = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($kitchentype);
+    }
+
+
+}
+
